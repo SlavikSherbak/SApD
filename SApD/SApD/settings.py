7504c8a3430d54e2fd54654d155f1caa57099214
Вячеслав <Misternictof@gmail.com>
2022-04-10 19:48:47 +0300

    1

--- SApD/SApD/settings.py
+++ SApD/SApD/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from operator import truth
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#xpo)i%a-1cy&r#-&i&ockotjnp=)0ggyndj1fmv&9e#3i&0@1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
